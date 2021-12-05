1c64e9de007595024856f64c6587453dfffc13c0
Ananth Vankipuram <vsap78@gmail.com>
2021-12-05 15:21:21 -0800

    adding ex5

---
+++ ex5.py
@@ -0,0 +1,19 @@
+my_name = 'Ananth Vankipuram'
+my_age = 43 # Not a lie
+my_height = 70 # inches
+my_weight = 160 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy")
+print("Actually thats not too heavy")
+print(f"Hes got {my_eyes} eyes and {my_hair} hair")
+print(f"His teeth are usually {my_teeth} depending on the coffee")
+
+# this line is tricky
+
+total = my_age + my_height + my_weight
+print(f"If add my {my_age}, {my_height}, {my_weight} I get a {total}.")
