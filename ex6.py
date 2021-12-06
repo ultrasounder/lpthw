b40b6c4f0f6bd5da75f50a166b9071eb6bb7ff60
Ananth Vankipuram <vsap78@gmail.com>
2021-12-05 21:10:28 -0800

    adding ex5

---
+++ ex6.py
@@ -0,0 +1,15 @@
+types_of_people = 10
+x= f"There are {types_of_people}"
+binary = "binary"
+do_not = "dont"
+y = f"Those who know {binary} and those who dont{do_not}"
+
+print(x)
+
+print(y)
+
+print(f"I said: {x}")
+print(f"I said: {y}")
+
+hillarious = False
+joke_evaluation = "Isnt"
