fbb9c2530a0ea9146ad8e9f4a3dd645c41821328
Ananth Vankipuram <vsap78@gmail.com>
2021-12-07 22:52:36 -0800

    adding more files

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("how old are you?", end = ' ')
+age = input()
+print("how tall are you?", end = ' ')
+height = input()
+print("how much do you weight", end = ' ')
+weight = input()
+
+print(f"so, You are {age} years old, you are {height} tall and you weight {weight}")
